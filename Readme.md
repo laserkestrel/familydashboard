5cc21d75aa1f1c3acd9b5b94a9b501f3e005176d
Laser Kestrel <tidyandy@gmx.com>
2024-03-01 16:41:18 +0000

    chatgpt suggested js

--- Readme.md
+++ Readme.md
@@ -6,3 +6,7 @@ Landing<BR>
 http://<BRIDGEIP>/api/<APIKEY>/sensors/203<BR>
 Noidea<BR>
 http://<BRIDGEIP>/api/<APIKEY>/sensors/206<BR>
+<BR><BR>
+docker build https://github.com/laserkestrel/familydashboard.git<BR>
+docker ps # lists all images running<BR>
+docker run -d -p 3000:3000 your_image_name<BR>
---
+++ index.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+const PORT = 3000;
+
+// Fetch data from the Philips Hue API
+async function fetchDataFromHue() {
+    try {
+        const response = await axios.get('YOUR_HUE_API_URL');
+        return response.data; // Assuming the response is JSON
+    } catch (error) {
+        console.error('Error fetching data from Hue API:', error);
+        return null;
+    }
+}
+
+// Route to render the HTML page
+app.get('/', async (req, res) => {
+    const hueData = await fetchDataFromHue();
+    // Render HTML using EJS template
+    ejs.renderFile('template.ejs', { hueData }, (err, html) => {
+        if (err) {
+            console.error('Error rendering template:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(html);
+        }
+    });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
